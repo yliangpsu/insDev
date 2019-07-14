9e4e9466545c0610e6f20ffe066d452671305562
YueLiang <yueliangpsu@gmail.com>
2019-07-13 20:11:53 -0400

    create image

--- Insta/urls.py
+++ Insta/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+
+from django.contrib import admin
+from django.urls import path, include
+from Insta.views import PostView, PostDetail
 
 urlpatterns = [
-    path('', views.HelloDjango.as_view(), name='home'),
+    path('', PostView.as_view(), name='home'),
+    path('post/<int:pk>', PostDetail.as_view(), name='post_detail'),
 ]
